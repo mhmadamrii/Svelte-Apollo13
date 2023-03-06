aa9f2ddfcb648a7d59434124bbbd4e403b14a4b0
mhmadamrii <interceptorghost4@gmail.com>
2023-03-06 19:08:11 +0700

    introduction

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,28 @@
 <script>
   let name = "World"
+  let src = "favicon.png"
+  let count = 0
+
+  const incrementCounter = () => {
+    count += 1;
+  }
 </script>
 
 <main>
   <h1>Hello {name}</h1>
+  <h2>counter: {count}</h2>
+  <button on:click={incrementCounter}>increment</button>
+  <img src={src} alt="svelte" />
 </main>
 
 <style>
+
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
   h1 {
     font-size: 72px;
     text-align: center;
